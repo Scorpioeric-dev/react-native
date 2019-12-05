6836dbab4f26d0a967eab1875448460b97b10606
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-12-05 15:31:31 -0700

    Map display w/ locator

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MapView, Permissions, Location } from "expo";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: null,
+    };
+    this._getLocationAsync()  
+      
+  }
+  //access devices location via the GPS
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.Location);
+    if (status !== "granted") console.log("Permission denied");
+
+    let Location = await Location.getCurrentPositionAsync({
+      enabledHighAccuracy: true
+    });
+    let region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.045
+    };
+    this.setState({region:region});
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+        <MapView
+          initialRegion={this.state.region}
+          showsUserLocation={true}
+          showsCompass={true}
+          rotateEnabled={false}
+          style={{ flex: 1 }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
